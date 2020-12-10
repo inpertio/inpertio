7f7aee559f3480d9dbb8dd1880233f4d714c543f
Denis Zhdanov <denzhdanov@gmail.com>
2020-12-10 12:14:39 +0800

    #8 Define basic server interfaces
    Addressing review comments

--- server/impl/src/main/kotlin/org/inpertio/server/resource/service/impl/ResourceServiceImpl.kt
+++ server/impl/src/main/kotlin/org/inpertio/server/resource/service/impl/ResourceServiceImpl.kt
@@ -5,17 +5,18 @@ import org.inpertio.server.resource.service.ResourceService
 import org.inpertio.server.util.ProcessingResult
 import org.springframework.stereotype.Component
 import java.io.File
+import java.io.InputStream
 
 @Component
 class ResourceServiceImpl(
     private val gitService: GitService
 ) : ResourceService {
 
-    override fun getResource(branch: String, resourcePath: String): ProcessingResult<ByteArray, String> {
+    override fun getResource(branch: String, resourcePath: String): ProcessingResult<InputStream, String> {
         return gitService.withBranch(branch) { rootDir ->
             val resource = File(rootDir, resourcePath)
             if (resource.isFile) {
-                ProcessingResult.success(resource.readBytes())
+                ProcessingResult.success(resource.inputStream())
             } else {
                 ProcessingResult.failure("no resource at path '$resourcePath' is found in branch '$branch'")
             }
